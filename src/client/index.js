5ee42f90fb2e71add052704c34f6914dec494304
Kokou Kpadenou <kkokou@gmail.com>
2023-06-01 18:08:23 -0400

    fix: Fix functionality

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,14 @@
 import { checkForName } from "./js/nameChecker";
 import { handleSubmit } from "./js/formHandler";
 
-import './styles/resets.scss';
-import './styles/base.scss';
-import './styles/footer.scss';
-import './styles/form.scss';
-import './styles/header.scss';
-
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 console.log(checkForName);
 
 alert("Hello");
+
+export { checkForName, handleSubmit };
