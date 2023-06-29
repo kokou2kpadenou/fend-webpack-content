370417ea5930648527ca5630f884963c3287cd77
Kokou Kpadenou <kkokou@gmail.com>
2023-06-29 13:43:05 -0400

    chore: apply DRY principe to webpack

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: [ "./src/client/index.js" ],
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,56 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+// const path = require("path");
+// const webpack = require("webpack");
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const BundleAnalyzerPlugin =
+//   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+//
+// module.exports = {
+//   mode: "development",
+//   devtool: "source-map",
+//   entry: "./src/client/index.js",
+//   output: {
+//     libraryTarget: "var",
+//     library: "Client",
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: "babel-loader",
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: ["style-loader", "css-loader", "sass-loader"],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebPackPlugin({
+//       template: "./src/client/views/index.html",
+//       filename: "./index.html",
+//     }),
+//     new CleanWebpackPlugin({
+//       dry: true,
+//       verbose: true,
+//       cleanStaleWebpackAssets: true,
+//       protectWebpackAssets: false,
+//     }),
+//     new BundleAnalyzerPlugin(),
+//   ],
+// };
 
-module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  entry: "./src/client/index.js",
-  output: {
-    libraryTarget: "var",
-    library: "Client",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const common = require('./webpack.common');
+
+const dev = {
+  mode: 'development',
+  devtool: 'source-map',
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/client/views/index.html",
-      filename: "./index.html",
-    }),
     new CleanWebpackPlugin({
       dry: true,
       verbose: true,
@@ -38,5 +58,8 @@ module.exports = {
       protectWebpackAssets: false,
     }),
     new BundleAnalyzerPlugin(),
+    new ESLintPlugin(),
   ],
 };
+
+module.exports = merge(common, dev);
